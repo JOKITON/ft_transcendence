12da97c1f8082e1623115c030af584bd22dac009
Jokin <jokinaizpuru28@gmail.com>
2024-06-09 22:36:56 +0200

    Set Django to show default page

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -32,10 +32,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'backend.urls'
 
+# Add the 'templates' directory to the TEMPLATES setting
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'backend', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- app/backend/views.py
+++ app/backend/views.py
@@ -2,14 +2,8 @@ from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 
 
-def image_upload(request):
-    if request.method == "POST" and request.FILES["image_file"]:
-        image_file = request.FILES["image_file"]
-        fs = FileSystemStorage()
-        filename = fs.save(image_file.name, image_file)
-        image_url = fs.url(filename)
-        print(image_url)
-        return render(request, "index.html", {
-            "image_url": image_url
-        })
-    return render(request, "index.html")
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
