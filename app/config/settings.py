de33f708d6c977825a5995138936735c5bd59279
Jokin <jokinaizpuru28@gmail.com>
2024-06-10 20:19:34 +0200

    Create Pong webpage with Boostrap

--- app/config/settings.py
+++ app/config/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/app/web/staticfiles'
+STATIC_ROOT = '/home/app/web/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/app/web/mediafiles'
+MEDIA_ROOT = '/home/app/web/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
