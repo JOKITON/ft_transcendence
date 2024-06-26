406052a4975d414280bf9e9c39faae7cbce20a90
Jokin <jokinaizpuru28@gmail.com>
2024-06-26 16:35:20 +0200

    Add Vue enviroment files & dependencies

---
+++ tests/hello-world/src/components/Index.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a
+        class="navbar-brand"
+        href="home"
+        data-link="home"
+      >SPA Example</a>
+      <div
+        id="navbarNav"
+        class="collapse navbar-collapse"
+      >
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              href="home"
+              data-link="home"
+            >Home</a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              href="login"
+              data-link="login"
+            >Login</a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              href="register"
+              data-link="register"
+            >Register</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pageIndex',
+}
+</script>
+
+<style scoped>
+</style>
---
+++ tests/hello-world/webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]?[hash]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+    },
+    extensions: ['*', '.js', '.vue', '.json'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      title: 'My App',
+    }),
+    new VueLoaderPlugin(),
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+};
