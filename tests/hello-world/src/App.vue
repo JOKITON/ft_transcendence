b8c5bcae486c21594a20a43f6f6056ab8bf94bb3
Jokin <99480973+JOKITON@users.noreply.github.com>
2024-06-26 15:13:03 +0000

    Start parsing from JS to Vue syntax

--- tests/hello-world/src/App.vue
+++ tests/hello-world/src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <div>
-    <Index/>
-  </div>
+  <div class="container">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="home" data-link="home">SPA Example</a>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="home" data-link="home">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="login" data-link="login">Login</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="register" data-link="register">Register</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
 </template>
 
 <script>
-import Index from './components/Index.vue'
 
 export default {
   name: 'App',
-  components: {
-    Index
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- tests/hello-world/src/main.js
+++ tests/hello-world/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/styles.css";
 
 createApp(App).mount('#app')
