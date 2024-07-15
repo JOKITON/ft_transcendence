2aeef3cff8368c8fd9284b84d4578f5ed8487233
JOKITON <jokinaizpuru28@gmail.com>
2024-07-15 19:21:50 +0200

    Merge changes from main branch to DJango & Vue - Merge was made manually, since a lot of files   were placed in different directories!

--- src/frontend/login/login/src/utils/auth.js
+++ src/frontend/login/login/src/utils/auth.js
@@ -1,23 +1,53 @@
 // utils/auth.js
-import { api } from "../main";
-// import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
+import api from "./api";
 
+export function setAuthHeader(accessToken, refreshToken) {
+  if (accessToken && refreshToken) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+    Cookies.set('refresh_token', refreshToken, { secure: true, sameSite: 'Lax' });
+    Cookies.set('access_token', accessToken, { secure: true, sameSite: 'Lax' });
+  }
+}
+
+// Function to set Authorization header
+export function setAuthHeaderFromCookie() {
+  const accessToken = Cookies.get("access_token");
+  if (accessToken) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+  } else {
+    delete api.defaults.headers.common["Authorization"];
+  }
+}
+
+// Function to refresh auth token
 export async function refreshAuthToken() {
   try {
-    const response = await api.post("/api/token/refresh/");
-    console.log(response);
+    const refreshToken = Cookies.get('refresh_token');
+    if (!refreshToken) {
+      throw new Error('No refresh token available');
+    }
+
+    const response = await api.post('token/refresh/', { refresh: refreshToken });
+    const newAccessToken = response.data.access;
+    const newRefreshToken = response.data.refresh;
+
+    // Update the Authorization header with the new token
+    setAuthHeader(newAccessToken, newRefreshToken);
+
     return true;
-  }
-  catch (error) {
+  } catch (error) {
     console.error(
-      "Login error:",
+      "Refresh token error:",
       error.response ? error.response.data : error.message
     );
-    // Handle login error (e.g., show error message to user)
+    // Handle refresh token error (e.g., log out the user)
     alert(
-      "Login failed. " +
+      "Session expired. Please log in again. " +
         (error.response ? error.response.data.detail : error.message)
     );
     return false;
   }
 }
+
+export { api };
--- src/frontend/login/login/src/utils/csrf.js
+++ src/frontend/login/login/src/utils/csrf.js
@@ -1,32 +1,25 @@
 import Cookies from 'js-cookie';
-import { api } from '../main'; // Import the api instance from main.js
-
-// Function to get CSRF token from cookies using js-cookie
-function getCsrfToken() {
-    return Cookies.get('csrftoken');
-}
+import api from './api'; // Import the api instance from main.js
 
 // Function to check if CSRF cookie is valid
 async function checkCSRF() {
-    const csrfToken = getCsrfToken();
-  
-    if (csrfToken) {
-        try {
-            const response = await api.get("/api/csrf/");
-            if (response.data.status === "off") {
-                console.log("CSRF token is invalid or expired");
-                Cookies.remove('csrftoken', { path: '/' });
-                return false;
-            }
-            api.defaults.headers.common['X-CSRFToken'] = response.headers["x-csrftoken"];
-            return true;
-        } catch (error) {
-            console.error("Error checking CSRF token:", error);
+    try {
+        const response = await api.get("csrf/check/");
+        if (response.data.status === "off") {
+            console.log("CSRF token is invalid or expired");
             Cookies.remove('csrftoken', { path: '/' });
             return false;
         }
-    }
+        const csrfToken = response.data.csrftoken;
+        console.log(csrfToken);
+        api.defaults.headers.common['X-CSRFToken'] = csrfToken;
+        Cookies.set('csrftoken', csrfToken, { secure: true, sameSite: 'Lax' });
+        return true;
+    } catch (error) {
+        console.error("Error checking CSRF token:", error);
+        Cookies.remove('csrftoken', { path: '/' });
     return false;
+    }
 }
 
 // Function to fetch and set CSRF token
@@ -40,11 +33,13 @@ export default async function fetchAndSetCsrfToken() {
         console.log('No valid CSRF tokens were found');
 
         // Fetch new CSRF token from the server
-        const response = await api.get("/api/csrf/");
+        const response = await api.get("csrf/");
         const csrfToken = response.headers["x-csrftoken"];
 
+        console.log(csrfToken);
         // Set CSRF token in Axios defaults and cookies
         api.defaults.headers.common['X-CSRFToken'] = csrfToken;
+        Cookies.set('csrftoken', csrfToken, { secure: true, sameSite: 'Lax' });
     } catch (error) {
         console.error("Error fetching CSRF token:", error);
         throw error;
