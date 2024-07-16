035f25c3018b71b91c8b966fea988d959b976d83
dugonzal <ciclocontinuo2@gmail.com>
2024-07-16 17:56:34 +0200

    tenemos logger

---
+++ src/backend/pong/src/server/Server.ts
@@ -0,0 +1,15 @@
+/*import { Server } from "socket.io";
+
+const io = new Server(server);
+io.listen(3000);
+
+// mas o menos asi iria la conexion por socket.io con el frontend
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+
+*/
