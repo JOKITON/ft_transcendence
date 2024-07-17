20a0910c069776fffcfc5a4cf6c15aa57f761b38
dugonzal <ciclocontinuo2@gmail.com>
2024-07-17 21:56:49 +0200

    sin mas hora de empezar

--- src/frontend/ft_trancendence/ft_trancendence/src/main.ts
+++ src/frontend/ft_trancendence/ft_trancendence/src/main.ts
@@ -1,16 +1,12 @@
-// asthetic imports
 import './assets/css/styles.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
 import fetchAndSetCsrfToken from './utils/csrf'
 import { setAuthHeaderFromCookie, api } from './utils/auth' // Import the functions from utils/auth.js
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const app = createApp(App)
 
