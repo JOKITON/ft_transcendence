dd3a130e7ef46595a996b22311d1c52bee61b7be
Santi <sperez-p@DESKTOP-AFMVINK>
2024-09-06 19:54:04 +0200

    primera

--- src/backend/auth/auth/config/settings.py
+++ src/backend/auth/auth/config/settings.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
+    # "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
