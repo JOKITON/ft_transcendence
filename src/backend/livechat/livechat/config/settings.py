1c19b6b6a2e304db9b3207b29b7588f44ce95502
dugonzal <dugonzal@protonmail.com>
2024-09-25 08:12:40 +0200

    Fix(live chat), channels add settings.py

--- src/backend/livechat/livechat/config/settings.py
+++ src/backend/livechat/livechat/config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
+    "channels",
     "UserModel",
     "livechat",
 ]
