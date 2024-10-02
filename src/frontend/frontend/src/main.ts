ecfde812f5d7f9f0122bfa23dd498cc03da2c03f
i-baines <ignacio@DESKTOP-F5T5DRH>
2024-10-02 19:01:30 +0200

    chat prueba

--- src/frontend/frontend/src/main.ts
+++ src/frontend/frontend/src/main.ts
@@ -6,6 +6,7 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import api from './plugins/api'
+import Chat from 'vue3-beautiful-chat'
 
 import 'vue3-carousel/dist/carousel.css'
 import './assets/css/styles.css'
@@ -13,6 +14,7 @@ import './assets/css/styles.css'
 const app = createApp(App)
 
 app.use(api)
+app.use(Chat)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
